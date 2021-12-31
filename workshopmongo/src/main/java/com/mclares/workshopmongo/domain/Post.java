1a218f76ab718b3c0b5cf35ae9df882e04d97ce1
Mclares <msc.09.wylde@gmail.com>
2021-12-31 18:11:15 -0300

    Referenciando os posts do usuário.

--- workshopmongo/src/main/java/com/mclares/workshopmongo/domain/Post.java
+++ workshopmongo/src/main/java/com/mclares/workshopmongo/domain/Post.java
@@ -13,12 +13,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 @Document
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
