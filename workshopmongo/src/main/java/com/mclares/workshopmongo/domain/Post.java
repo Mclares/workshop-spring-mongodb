54a4c19e35b7e7901c579a00691687ebadde1490
Mclares <msc.09.wylde@gmail.com>
2022-01-02 15:26:09 -0300

    Acrescentando comentários aos posts.

--- workshopmongo/src/main/java/com/mclares/workshopmongo/domain/Post.java
+++ workshopmongo/src/main/java/com/mclares/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.mclares.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.mclares.workshopmongo.dto.AuthorDTO;
+import com.mclares.workshopmongo.dto.CommentDTO;
 
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -23,6 +26,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -74,5 +79,13 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 }
