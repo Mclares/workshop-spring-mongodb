1c3c7ed8bd9e0bd268a47178ae174ff435cf58de
Mclares <msc.09.wylde@gmail.com>
2022-01-03 19:04:49 -0300

    Consulta com vários critérios.

--- workshopmongo/src/main/java/com/mclares/workshopmongo/controller/PostController.java
+++ workshopmongo/src/main/java/com/mclares/workshopmongo/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.mclares.workshopmongo.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +33,17 @@ public class PostController {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
-	} 
+	}
+	
+	@GetMapping(value="/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue = "") String text,
+			@RequestParam(value="minDate", defaultValue = "") String minDate,
+			@RequestParam(value="MaxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 }
